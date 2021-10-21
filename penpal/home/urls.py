1ac2c6501e036fb58195490a5794d037e065f849
yasinahmad12 <yasin.inf18@student.unu-jogja.ac.id>
2021-10-21 19:46:28 +0700

    lenjutan proyek

--- penpal/home/urls.py
+++ penpal/home/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path ,include 
 from django.urls.resolvers import URLPattern
 
+
 from . import views
+from .views import *
 
 urlpatterns = [
     path('', views.index),
+    path('', views.home , name="home")
 ]
--- penpal/home/views.py
+++ penpal/home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index (req):
-    return render (req,"index.html")
+    return render (req,"base.html")
+
+def home (request):
+    return render( request= 'home.html')
